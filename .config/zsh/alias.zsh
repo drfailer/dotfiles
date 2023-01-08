24fc913a5dd8b3600c0c945fb534407604db6bdf
drfailer <drfailer.42@gmail.com>
2023-01-08 08:25:29 +0100

    git alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -19,19 +19,31 @@ alias vpn='sudo openvpn ~/.config/vpn/vpn-student-tcp.ovpn'
 alias zz='zathura'
 alias so='source ~/.config/zsh/.zshrc'
 alias tmuxh='nvim ~/.config/tmux/tmux.md'
+alias lg='lazygit'
 
 # book marks:
-alias gc='cd $HOME/Documents/cours'
+alias gco='cd $HOME/Documents/cours'
 alias gd='cd $HOME/Downloads'
 alias gp='cd $HOME/Documents/dev/progs'
+alias gS='cd $HOME/.config/zsh/scripts'
 alias gdd='cd $HOME/Documents/dev/'
 alias gC='cd $HOME/.config/'
 alias gsk='cd $HOME/.config/suckless-builds/'
 alias gskm='cd $HOME/.config/suckless-builds/dmenu/'
 alias gskt='cd $HOME/.config/suckless-builds/st/'
 alias gz='cd $HOME/.config/zsh/'
-alias gm='cd $HOME/.local/share/music/'
+alias gM='cd $HOME/.local/share/music/'
 alias gv='cd $HOME/.config/nvim/'
+
+# git
+alias g='git'
+alias ga='git add -A'
+alias gc='git commit -m '
+alias gca='git commit --amend -m '
+alias gm='git merge '
+alias gb='git branch '
+alias gba='git branch -a'
+alias gck='git checkout '
 
 # fzutils
 alias mvv='fzutils mv'
@@ -57,10 +69,25 @@ alias jcompclass='javac -Xlint:all'
 alias screenshot='gnome-screenshot --interactive'
 alias reckeys='screenkey -p fixed -g 90%x7%+61%-2%'
 
+# fzf directory
 cdd() {
   if [ $# -eq 1 ]; then
     cd $(fzutils cd $1)
   else
     cd $(fzutils cd)
+  fi
+}
+
+# git new branch
+gnb() {
+  if [ $# -eq 1 ]; then
+    # track remote branch if exists
+    if [ ! -z $(git branch -r | grep $1) ]; then
+      git branch --track $1 origin/$1
+    else
+      git branch $1
+    fi
+  else
+    echo "ERROR: branch name required"
   fi
 }
